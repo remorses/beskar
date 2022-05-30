8378f7c062fdac57b253802386621b87a5ac0a38
remorses <beats.by.morse@gmail.com>
2022-05-30 12:58:59 +0200

    fix link

--- src/landing/Link.tsx
+++ src/landing/Link.tsx
@@ -1,23 +1,21 @@
 import clsx from 'clsx'
 import NextLink from 'next/link'
-import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
+import React, { ComponentPropsWithoutRef, forwardRef, Fragment } from 'react'
 
 export type LinkProps = ComponentPropsWithoutRef<'a'>
 
 export const Link = forwardRef<any, LinkProps>(
     ({ href = '', children, className, ...props }, ref) => {
         const isExternal = !href.startsWith('/') && !href.startsWith('#')
-        const Wrapper = !isExternal ? NextLink : 'span'
+        const Wrapper: any = !isExternal ? NextLink : Fragment
         if (isExternal && !props.target) {
             props.target = '_blank'
         }
         return (
-            <Wrapper
-                href={href || ''}
-                {...(isExternal ? {} : { passHref: true })}
-            >
+            <Wrapper {...(isExternal ? {} : { passHref: true, href })}>
                 <a
                     ref={ref}
+                    href={href || ''}
                     className={clsx(
                         'appearance-none max-w-max relative no-underline',
                         'font-medium !border-white/0 truncate cursor-pointer',
