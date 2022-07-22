720b3ed8430d3cc6accc014b8b9882a394b910c4
remorses <beats.by.morse@gmail.com>
2022-07-22 21:34:27 +0200

    added nextui

---
+++ src/nextui.tsx
@@ -0,0 +1,66 @@
+import '@app/styles/index.css'
+
+// @ts-ignore
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+import 'baby-i-am-faded/styles.css'
+
+import colors from 'tailwindcss/colors'
+
+colors.gray = colors.neutral as any
+
+const formattedColors = Object.fromEntries(
+    Object.keys(colors)
+        .flatMap((colorName) => {
+            const colorObj = colors[colorName]
+            if (typeof colorObj === 'string') {
+                return [[colorName, colorObj]]
+            }
+            return Object.keys(colorObj).map((colorNumber) => {
+                if (!colorName) {
+                    return []
+                }
+                return [`${colorName}-${colorNumber}`, colorObj[colorNumber]]
+            })
+        })
+        .filter(Boolean),
+)
+
+// console.log(formattedColors)
+const lightTheme = createTheme({
+    type: 'light',
+    className: 'light',
+    theme: {
+        colors: {
+            ...formattedColors,
+            backgroundContrast: '#fff',
+        },
+    },
+})
+
+const darkTheme = createTheme({
+    type: 'dark',
+    className: 'dark',
+    theme: {
+        colors: {
+            ...formattedColors,
+            backgroundContrast: colors.neutral['800'],
+        },
+    },
+})
+
+// disable dead code elimination
+Object.assign(
+    {},
+    {
+        lightTheme,
+        darkTheme,
+    },
+)
+
+export function NextUiStuff({ children, ...rest }) {
+    return (
+        <NextUIProvider disableBaseline {...rest}>
+            {children}
+        </NextUIProvider>
+    )
+}
