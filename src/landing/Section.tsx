8a0a7890bb91344ae94404c43078e96ac8c45601
remorses <beats.by.morse@gmail.com>
2022-08-15 13:16:30 +0200

    nicer tables

--- src/landing/Section.tsx
+++ src/landing/Section.tsx
@@ -10,10 +10,11 @@ export const Section = ({
     children,
     bg,
     className,
-    degree = 2,
+    degree: _degree = 2,
     ...rest
 }: SectionProps) => {
     // const { bg } = rest
+    let degreeStr = _degree.toFixed(1)
     const styles: CSSProperties = {
         background: bg,
         width: '100%',
@@ -22,23 +23,24 @@ export const Section = ({
         right: 0,
         bottom: 0,
         position: 'absolute',
-        transform: `skewY(${degree}deg)`,
+        transform: `skewY(${degreeStr}deg)`,
         transformOrigin: 'center',
     }
+
     return (
         <div className='relative' {...rest}>
             <div style={{ ...styles }} />
             <div
                 style={{
                     ...styles,
-                    transform: `skewY(${degree + 2}deg)`,
+                    transform: `skewY(${degreeStr + 2}deg)`,
                     opacity: 0.2,
                     transformOrigin: '60%',
                 }}
             />
             <div
                 style={{
-                    transform: `skewY(${degree + 3}deg)`,
+                    transform: `skewY(${degreeStr + 3}deg)`,
                     transformOrigin: '70%',
                     opacity: 0.1,
                     ...styles,
