8c71f5994ebcdef79dd472c196fb953cd0d99b59
remorses <beats.by.morse@gmail.com>
2022-08-21 15:31:04 +0200

    MyTestimonialLogos using page container

--- src/landing/TestimonialLogos.tsx
+++ src/landing/TestimonialLogos.tsx
@@ -1,5 +1,6 @@
 import { Faded } from 'baby-i-am-faded'
 import clsx from 'clsx'
+import { PageContainer } from './PageContainer'
 
 type Props = {
     items: {
@@ -12,36 +13,38 @@ type Props = {
 }
 export function TestimonialLogos({ items, invert = false, className }: Props) {
     return (
-        <div
-            className={clsx(
-                'flex flex-col justify-between items-center space-y-14',
-                'px-6 md:flex-row md:space-y-0',
-                className,
-            )}
-        >
-            {items.map((x) => {
-                const LinkOrNot = x.href ? 'a' : 'div'
-                return (
-                    <LinkOrNot
-                        key={x.src}
-                        style={{
-                            opacity: x.opacity,
-                            ...(invert && { filter: 'invert()' }),
-                        }}
-                        {...(x.href && {
-                            href: x.href || '',
-                            target: '_blank',
-                        })}
-                        className='block appearance-none'
-                    >
-                        <img
-                            src={x.src}
-                            alt='logo'
-                            className='max-w-[180px] max-h-[40px]'
-                        />
-                    </LinkOrNot>
-                )
-            })}
-        </div>
+        <PageContainer>
+            <div
+                className={clsx(
+                    'flex flex-col justify-around flex-1 items-center ',
+                    'md:flex-row gap-14',
+                    className,
+                )}
+            >
+                {items.map((x) => {
+                    const LinkOrNot = x.href ? 'a' : 'div'
+                    return (
+                        <LinkOrNot
+                            key={x.src}
+                            style={{
+                                opacity: x.opacity,
+                                ...(invert && { filter: 'invert()' }),
+                            }}
+                            {...(x.href && {
+                                href: x.href || '',
+                                target: '_blank',
+                            })}
+                            className='block appearance-none'
+                        >
+                            <img
+                                src={x.src}
+                                alt='logo'
+                                className='max-w-[180px] max-h-[40px]'
+                            />
+                        </LinkOrNot>
+                    )
+                })}
+            </div>
+        </PageContainer>
     )
 }
