e41ff7debf26811d42c80db5fed80fd88c6c5f2f
remorses <beats.by.morse@gmail.com>
2022-08-29 12:22:33 +0200

    more rounded block

--- src/dashboard/Block.tsx
+++ src/dashboard/Block.tsx
@@ -7,7 +7,7 @@ export const Block = ({ className, children, ...rest }: BlockProps) => {
     return (
         <div
             className={clsx(
-                'relative flex flex-col min-w-0 overflow-x-auto p-7 px-8 space-y-6 shadow rounded',
+                'relative flex flex-col min-w-0 overflow-x-auto p-7 px-8 space-y-6 shadow rounded-md',
                 'bg-white dark:bg-gray-800',
                 className,
             )}
