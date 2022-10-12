c41d3f3cf65f42a342704a866d80a734523f21f4
remorses <beats.by.morse@gmail.com>
2022-10-12 13:30:54 +0200

    fix useCopyToClipboard

--- src/dashboard/CopyButton.tsx
+++ src/dashboard/CopyButton.tsx
@@ -31,33 +31,15 @@ export const CopyButton = ({
 
 export const useCopyToClipboard = (text: string) => {
     const copyToClipboard = (str: string) => {
-        const el = document.createElement('textarea')
-        el.value = str
-        el.setAttribute('readonly', '')
-        el.style.position = 'absolute'
-        el.style.left = '-9999px'
-        document.body.appendChild(el)
-        const selection = document.getSelection()
-        if (!selection) {
-            return false
-        }
-        const selected =
-            selection.rangeCount > 0 ? selection.getRangeAt(0) : false
-        el.select()
-        const success = document.execCommand('copy')
-        document.body.removeChild(el)
-        if (selected) {
-            selection.removeAllRanges()
-            selection.addRange(selected)
-        }
-        return success
+        navigator.clipboard.writeText(str)
+        return true
     }
 
     const [copied, setCopied] = React.useState(false)
 
-    const copy = React.useCallback(() => {
-        if (!copied) setCopied(copyToClipboard(text))
-    }, [text])
+    const copy = () => {
+        setCopied(copyToClipboard(text))
+    }
     React.useEffect(() => {
         const id = setTimeout(() => setCopied(false), 1000)
         return () => {
