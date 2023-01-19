c2c42a9e2fc62c4b1be3b37912207ef1848abce9
remorses <beats.by.morse@gmail.com>
2023-01-19 09:08:05 +0100

    nn

--- src/analytics/components/badge-select.tsx
+++ src/analytics/components/badge-select.tsx
@@ -16,7 +16,7 @@ export default function BadgeSelect({
             option === selected
               ? "bg-gray-200 dark:bg-gray-700"
               : "bg-gray-50 hover:bg-gray-100 dark:hover:bg-gray-800 dark:bg-gray-900 "
-          } px-2 sm:px-3 py-1 rounded-md text-sm font-medium capitalize active:scale-90 transition-all duration-75`}
+          } px-2 sm:px-3 py-1 rounded-md text-sm font-medium capitalize active:scale-95 transition-all duration-75`}
           onClick={() => selectAction(option)}
         >
           {option}
