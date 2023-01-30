13167c95cf260ae7e9ff0d071bdd67e24031710c
remorses <beats.by.morse@gmail.com>
2023-01-30 23:28:45 +0100

    more options

--- src/Select.tsx
+++ src/Select.tsx
@@ -23,7 +23,7 @@ export type SelectProps = {
     value?: string
     placeholder?: string
     onChange: (x: string) => void
-    options: { value: string; name?: ReactNode }[]
+    options: { value: string; name?: ReactNode; whenButton?: ReactNode }[]
     endButton?: ReactNode
     useAutoGradientIcons?: boolean
     isLoading?: boolean
@@ -36,138 +36,150 @@ export const Select: React.FC<
     SelectProps & { children?: ReactNode; ref?: any }
 > & {
     SelectButton: typeof SelectButton
-} = forwardRef<any, any>(function (
-    {
-        onChange,
-        value,
-        endButton = null,
-        placeholder = 'Loading',
-        options,
-        className = '',
-        isLoading = false,
-        useAutoGradientIcons = false,
-    },
-    ref,
-) {
-    const router = useRouter()
-    const orgId = (router.query.orgId || '') as string
+} = forwardRef<any, SelectProps & { children?: ReactNode; ref?: any }>(
+    function (
+        {
+            onChange,
+            value,
+            endButton = null,
+            placeholder = 'Loading',
+            options,
+            className = '',
+            isLoading = false,
+            useAutoGradientIcons = false,
+        },
+        ref,
+    ) {
+        const router = useRouter()
+        const orgId = (router.query.orgId || '') as string
 
-    const hoverClasses = 'hover:bg-gray-100 hover:dark:bg-gray-600 rounded mx-2'
-    const current = options.find((x) => x.value === value)
-    return (
-        <>
-            <Listbox value={orgId} onChange={onChange}>
-                <div className={classNames('relative')}>
-                    <Listbox.Button
-                        ref={ref as any}
-                        as='button'
-                        className={classNames(
-                            'relative w-full py-[8px] pl-3 pr-10 text-left bg-white rounded-lg shadow border dark:bg-gray-700 focus:outline-none focus-visible:ring-1 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-gray-300 focus-visible:ring-offset-1 sm:text-sm',
-                            className,
-                        )}
-                    >
-                        <div
-                            aria-label='current org'
-                            className='flex items-center space-x-2 truncate font-medium'
-                        >
-                            {useAutoGradientIcons && (
-                                <GradientIcon
-                                    name={
-                                        isLoading
-                                            ? ''
-                                            : current?.name || current?.value
-                                    }
-                                />
-                            )}
-                            <span className=''>
-                                {isLoading
-                                    ? 'Loading'
-                                    : current?.name || placeholder}
-                            </span>
-                        </div>
-                        <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
-                            <SelectorIcon
-                                className='w-5 h-5 text-gray-400'
-                                aria-hidden='true'
-                            />
-                        </span>
-                    </Listbox.Button>
-                    <style jsx>{`
-                        @keyframes menuAppear {
-                            from {
-                                opacity: 0;
-                                transform: translate3d(0px, -2em, 0px) scale(0);
-                            }
-                            to {
-                                opacity: 1;
-                            }
-                        }
-                    `}</style>
-                    <Faded animationName='menuAppear' duration={120} cascade>
-                        <Listbox.Options
+        const hoverClasses =
+            'hover:bg-gray-100 hover:dark:bg-gray-600 rounded mx-2'
+        const current = options.find((x) => x.value === value)
+        return (
+            <>
+                <Listbox value={orgId} onChange={onChange}>
+                    <div className={classNames('relative')}>
+                        <Listbox.Button
+                            ref={ref as any}
+                            as='button'
                             className={classNames(
-                                'absolute z-50 gap-2 flex flex-col w-full py-2 mt-1 overflow-auto text-base bg-white rounded-md shadow-xl dark:bg-gray-700 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
+                                'relative w-full py-[8px] pl-3 pr-10 text-left bg-white rounded-lg shadow border dark:bg-gray-700 focus:outline-none focus-visible:ring-1 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-gray-300 focus-visible:ring-offset-1 sm:text-sm',
+                                className,
                             )}
                         >
-                            {options?.map((opt, idx) => (
-                                <Listbox.Option
-                                    key={opt.value}
-                                    className={({ active }) =>
-                                        classNames(
-                                            `cursor-pointer select-none relative py-[6px] `,
-                                            (opt.value === value || active) &&
-                                                'bg-gray-100 dark:bg-gray-600',
-                                            hoverClasses,
-                                        )
-                                    }
-                                    value={opt.value}
-                                >
-                                    {({ selected, active }) => (
-                                        <>
-                                            <div
-                                                className={classNames(
-                                                    `flex items-center space-x-2 pl-2 truncate font-medium`,
-                                                )}
-                                            >
-                                                {useAutoGradientIcons && (
-                                                    <GradientIcon
-                                                        name={
-                                                            opt?.name ||
-                                                            opt?.value
-                                                        }
-                                                    />
-                                                )}
-                                                <span className='text-sm'>
-                                                    {opt.name || opt.value}
-                                                </span>
-                                            </div>
-                                            {selected ? (
-                                                <span
+                            <div
+                                aria-label='current org'
+                                className='flex items-center space-x-2 truncate font-medium'
+                            >
+                                {useAutoGradientIcons && (
+                                    <GradientIcon
+                                        name={
+                                            isLoading
+                                                ? ''
+                                                : current?.name ||
+                                                  current?.value
+                                        }
+                                    />
+                                )}
+                                <span className='flex grow'>
+                                    {isLoading
+                                        ? 'Loading'
+                                        : current?.whenButton ||
+                                          current?.name ||
+                                          placeholder}
+                                </span>
+                            </div>
+                            <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
+                                <SelectorIcon
+                                    className='w-5 h-5 text-gray-400'
+                                    aria-hidden='true'
+                                />
+                            </span>
+                        </Listbox.Button>
+                        <style jsx>{`
+                            @keyframes menuAppear {
+                                from {
+                                    opacity: 0;
+                                    transform: translate3d(0px, -2em, 0px)
+                                        scale(0);
+                                }
+                                to {
+                                    opacity: 1;
+                                }
+                            }
+                        `}</style>
+                        <Faded
+                            animationName='menuAppear'
+                            duration={120}
+                            cascade
+                        >
+                            <Listbox.Options
+                                className={classNames(
+                                    'absolute z-50 gap-2 flex flex-col w-full py-2 mt-1 overflow-auto text-base bg-white rounded-md shadow-xl dark:bg-gray-700 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
+                                )}
+                            >
+                                {options?.map((opt, idx) => (
+                                    <Listbox.Option
+                                        key={opt.value}
+                                        className={({ active }) =>
+                                            classNames(
+                                                `cursor-pointer select-none relative py-[6px] `,
+                                                (opt.value === value ||
+                                                    active) &&
+                                                    'bg-gray-100 dark:bg-gray-600',
+                                                hoverClasses,
+                                            )
+                                        }
+                                        value={opt.value}
+                                    >
+                                        {({ selected, active }) => (
+                                            <>
+                                                <div
                                                     className={classNames(
-                                                        `absolute inset-y-0 right-1 flex items-center pl-3`,
-                                                        active
-                                                            ? 'text-amber-600'
-                                                            : 'text-amber-600',
+                                                        `flex items-center space-x-2 pl-2 truncate font-medium`,
                                                     )}
                                                 >
-                                                    <CheckIcon
-                                                        className='w-5 h-5'
-                                                        aria-hidden='true'
-                                                    />
-                                                </span>
-                                            ) : null}
-                                        </>
-                                    )}
-                                </Listbox.Option>
-                            ))}
-                            {endButton}
-                        </Listbox.Options>
-                    </Faded>
-                </div>
-            </Listbox>
-        </>
-    )
-}) as any
+                                                    {useAutoGradientIcons && (
+                                                        <GradientIcon
+                                                            name={
+                                                                opt?.name ||
+                                                                opt?.value
+                                                            }
+                                                        />
+                                                    )}
+                                                    <span className='flex grow text-sm'>
+                                                        {opt.name || opt.value}
+                                                    </span>
+                                                </div>
+                                                {selected ? (
+                                                    <span
+                                                        className={classNames(
+                                                            `absolute inset-y-0 right-1 flex items-center pl-3`,
+                                                            active
+                                                                ? 'text-amber-600'
+                                                                : 'text-amber-600',
+                                                        )}
+                                                    >
+                                                        <CheckIcon
+                                                            className='w-5 h-5'
+                                                            aria-hidden='true'
+                                                        />
+                                                    </span>
+                                                ) : null}
+                                            </>
+                                        )}
+                                    </Listbox.Option>
+                                ))}
+                                {endButton}
+                            </Listbox.Options>
+                        </Faded>
+                    </div>
+                </Listbox>
+            </>
+        )
+    },
+) as any
 
 Select.SelectButton = SelectButton
 
