bb2bcd9f6ceee2e1d24ed758fe74e45dcbf428cd
remorses <beats.by.morse@gmail.com>
2024-07-13 15:31:00 +0200

    try adding composite to beskar

--- src/_tailwind.ts
+++
@@ -1,7 +0,0 @@
-import resolveConfig from 'tailwindcss/resolveConfig'
-// import tailwindConfig from './tailwind.config.js'
-
-export function getConfig(tailwindConfig: any) {
-    const fullConfig = resolveConfig(resolveConfig(tailwindConfig))
-    return fullConfig
-}
